ef7cb026cf6e8ab415114ed69df83758407aeda8
Xin Liang <xin.liang@trademark.vision>
2017-08-05 15:57:00 +1000

    Fixes on email alert for due date

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -4,6 +4,6 @@ from django.core.mail import send_mail
 # Register your models here.
 
 def email_alert(msg, from_email, to_email_list):
-    subject = 'Bill Dashboard Email Alert'
+    subject = 'Bills Dashboard Email Alert'
 
     send_mail(subject, msg, from_email, to_email_list)
--- dashboard/management/commands/duedate_alert.py
+++ dashboard/management/commands/duedate_alert.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from datetime import datetime
 
 from django.core.management.base import BaseCommand, CommandError
 from dashboard.models import Bills
@@ -7,6 +8,7 @@ from dashboard.admin import email_alert
 
 def check_duedate(to_email_list, bill_name=None, alert_days=3):
     today = datetime.today()
+
     msg_base = "Bill %s is due on %s."
     from_email = "bills.dashboard@gmail.com"
 
@@ -17,24 +19,28 @@ def check_duedate(to_email_list, bill_name=None, alert_days=3):
         bills = [bill_name]
 
     for bill in bills:
-        duedates = Bills.objects.filter(name=bill).orderby('-due_date')
-        duedate = duedates[0]
+        duedates = Bills.objects.filter(name=bill).order_by('-due_date')
+        duedate = duedates[0].due_date.replace(tzinfo=None)
         time_delta = duedate - today
         if 0 < time_delta.days <= alert_days:
-            msg = msg_base % (bill_name, duedate.strftime('%Y-%m-%d'))
+            msg = msg_base % (bill, duedate.strftime('%Y-%m-%d'))
+            print 'check due date msg', msg
             email_alert(msg, from_email, to_email_list)
 
 
 class Command(BaseCommand):
 
     def add_arguments(self, parser):
-        parser.add_argument('bill_name', type=str, help='Bill Name')
-        parser.add_argument('email_list', type=str, help='List of emails separated by commas')
-        parser.add_argument('alert_days', type=int, help='Number of days ahead for alert')
+        parser.add_argument('--bill-name', dest="bill_name", type=str, help='Bill Name')
+        parser.add_argument('--email-list', dest="email_list", type=str, help='List of emails separated by commas')
+        parser.add_argument('--alert-days', dest="alert_days", type=int, help='Number of days ahead for alert')
 
     def handle(self, *arg, **options):
         bill_name = options.get('bill_name', None)
         email_list = options.get('email_list', []).split(',')
-        alert_days = options.get('alert_days', 3)
+        alert_days = options.get('alert_days')
+
+        if alert_days is None:
+            alert_days = 3
 
         check_duedate(email_list, bill_name=bill_name, alert_days=alert_days)
