88aa598f3e3410f37f07e8d22d8d617086324c3c
Xin Liang <xin.liang@trademark.vision>
2017-09-12 21:54:07 +1000

    Linked bills to users

--- dashboard/models.py
+++ dashboard/models.py
@@ -5,14 +5,17 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    alert_enabled = models.BooleanField(default=False)
+
 class Bills(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=200)
     due_date = models.DateTimeField()
     amount = models.FloatField()
 
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    alert_enabled = models.BooleanField(default=False)
+
 
--- dashboard/views.py
+++ dashboard/views.py
@@ -13,6 +13,9 @@ def default(request):
     return render(request, 'index.html')
 
 def dashboard(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect("/login")
+
     return render(request, 'dashboard.html')
     
 @csrf_exempt
@@ -21,10 +24,11 @@ def insert_bill(request):
     params = request.POST
     created_date = params.get('created_date')
     bill_name = params.get('bill_name')
-    due_date =  params.get('due_date')
+    due_date = params.get('due_date')
     amount = params.get('amount')
 
     obj, is_created = Bills.objects.get_or_create( \
+        owner=request.user,
         created=created_date,
         name=bill_name,
         due_date=due_date,
@@ -35,7 +39,7 @@ def insert_bill(request):
      
 @csrf_exempt
 def list_bills(request):
-    bills = list(Bills.objects.all().values_list('name', flat=True))
+    bills = list(Bills.objects.filter(owner=request.user).values_list('name', flat=True))
     bills = list(set(bills))
 
     return HttpResponse(json.dumps(bills), content_type="application/json")
@@ -43,10 +47,10 @@ def list_bills(request):
 @csrf_exempt
 def get_last_bills(request):
     data = dict()
-    bills = list(Bills.objects.all().values_list('name', flat=True))
+    bills = list(Bills.objects.filter(owner=request.user).values_list('name', flat=True))
     for b in bills:
         if b:
-            last_b = Bills.objects.filter(name=b).order_by('-due_date')[0]
+            last_b = Bills.objects.filter(owner=request.user, name=b).order_by('-due_date')[0]
             data[b] = {
                 'amount': last_b.amount,
                 'due_date': last_b.due_date.strftime('%Y-%m-%d')
@@ -58,10 +62,10 @@ def get_last_bills(request):
 @csrf_exempt
 def get_bills_summary(request):
     data = dict()
-    bills = list(Bills.objects.all().values_list('name', flat=True))
-    total = sum(list(Bills.objects.all().values_list('amount', flat=True)))
+    bills = list(Bills.objects.filter(owner=request.user).values_list('name', flat=True))
+    total = sum(list(Bills.objects.filter(owner=request.user).values_list('amount', flat=True)))
     for b in bills:
-        bill = Bills.objects.filter(name=b)
+        bill = Bills.objects.filter(owner=request.user, name=b)
         percent = sum(bill.values_list('amount', flat=True))/float(total) * 100
         data[b] = round(percent, 1)
 
@@ -73,7 +77,7 @@ def get_bill_overview(request):
     params = request.GET
     bill_name = params.get('bill_name')
     n_records = 12
-    last_year = Bills.objects.filter(name=bill_name).order_by('-due_date')
+    last_year = Bills.objects.filter(owner=request.user, name=bill_name).order_by('-due_date')
     if len(last_year) > n_records:
         last_year = last_year[:n_records]
     details = last_year.reverse()
@@ -99,16 +103,16 @@ def get_bill_history(request):
     history = None
     if from_date is not None:
         if to_date is not None:
-            history = Bills.objects.filter(name=bill_name, due_date__gte=from_date, due_date__lte=to_date) \
+            history = Bills.objects.filter(owner=request.user, name=bill_name, due_date__gte=from_date, due_date__lte=to_date) \
                 .order_by('due_date')
         else:
-            history = Bills.objects.filter(name=bill_name, due_date__gte=from_date).order_by('due_date')
+            history = Bills.objects.filter(owner=request.user, name=bill_name, due_date__gte=from_date).order_by('due_date')
 
     if to_date is not None and history is None:
-        history = Bills.objects.filter(name=bill_name, due_date__lte=to_date)
+        history = Bills.objects.filter(owner=request.user, name=bill_name, due_date__lte=to_date)
 
     if history is None:
-        history = Bills.objects.filter(name=bill_name)
+        history = Bills.objects.filter(owner=request.user, name=bill_name)
 
     dates = [d.strftime('%Y-%m-%d') for d in list(history.values_list('due_date', flat=True))]
     amounts = list(history.values_list('amount', flat=True))
